7cc0601c9f355f5a38f51967abd98284de2b91db
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-10-06 17:39:30 +0200

    refactor: rename headline to headline-top, adjust its responsiveness

--- src/components/landing/headline-top.tsx
+++ src/components/landing/headline-top.tsx
@@ -2,12 +2,12 @@ import Em from './empashized'
 
 export default function Headline() {
     return (
-        <section className='space-y-6 text-center'>
-            <h1 className='font-heading text-4xl font-bold'>
+        <section className='mx-auto max-w-4xl space-y-6 px-4 text-center'>
+            <h1 className='font-heading text-3xl font-bold leading-tight md:text-4xl'>
                 Spira inspires <Em>creativity</Em>, meaningful connections, <Em>personal growth</Em> and community
                 engagement
             </h1>
-            <p className='font-medium'>
+            <p className='text-pretty font-medium'>
                 We&apos;re all about finding solutions to social and environmental challenges while fostering
                 collaboration and building relationships for a better world.{' '}
                 <Em>Spira is your perfect match for everything you&apos;re seeking!</Em>
