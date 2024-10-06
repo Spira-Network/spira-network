6b20c67c035d1e345770b967011931fb5b18ae3f
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-10-06 17:35:39 +0200

    style: adjust texts and responsiveness for wheel

--- src/components/landing/wheel.tsx
+++ src/components/landing/wheel.tsx
@@ -1,32 +1,36 @@
 import Image from 'next/image'
 import { Button } from '../ui/button'
-// import wheel from '@/../public/wheel.png'
 
 export default function Wheel() {
     return (
-        <div className='flex flex-col items-center gap-4 text-white'>
-            <div className='relative size-96'>
-                <img
+        <div className='flex flex-col items-center gap-4 text-white sm:gap-6 md:gap-8'>
+            <div className='relative size-64 sm:size-80 md:size-96'>
+                <Image
                     src='/wheel.png'
-                    role='presentation'
-                    loading='lazy'
                     alt='A spinning wheel with a spiral pattern.'
                     className='object-cover'
+                    priority
+                    fill
                 />
             </div>
-            <h1 className='font-bold'>Wheel of Spheres</h1>
-            <h2 className='text-sm font-medium text-[#C9CEFF]'>Know thyself & Know thyEarth</h2>
-            <Button className='rounded-full bg-[#C9CEFF] px-5 font-heading text-[#121212]'>Start To Spin!</Button>
-            <h2 className='text-sm font-medium text-[#C9CEFF]'>
-                Join us and begin your journey Your wish is your command
+            <h1 className='text-center text-2xl font-bold sm:text-3xl md:text-4xl'>Spira Wheel of wills</h1>
+            <h2 className='text-center text-sm font-medium text-[#C9CEFF] sm:text-base md:text-lg'>
+                Know thyself & Know thyEarth
             </h2>
-            <ul className='list-["*"] space-y-3 text-sm font-medium text-white [&>li]:pl-2'>
-                <li>
+            <Button className='rounded-full bg-[#C9CEFF] px-5 py-2 font-heading text-sm text-[#121212] sm:px-6 sm:py-3 sm:text-base'>
+                Start To Spin!
+            </Button>
+            <h2 className='max-w-md text-center text-sm font-medium text-[#C9CEFF] sm:text-base md:text-lg'>
+                Join us and begin your journey
+                <br /> Your wish is your command
+            </h2>
+            <ul className='max-w-md list-["*"] space-y-3 text-sm font-medium text-white sm:space-y-4 sm:text-base'>
+                <li className='pl-2'>
                     Command <strong>Here & Now inspirations</strong>
                     <br />
                     to receive your daily frequency match guide message
                 </li>
-                <li>
+                <li className='pl-2'>
                     Command <strong>Time & Space inspirations</strong>
                     <br />
                     to receive your multidimensional blueprint map charts
