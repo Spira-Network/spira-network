c5d27484cc7cd824148d2ce9e7de7f27a5498560
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-10-06 21:41:51 +0200

    fix: correct wrong import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,28 @@
-'use client';
+'use client'
 
-import {Auth} from '@/components/auth';
-import {Background} from '@/components/background';
-import {Footer} from '@/components/footer';
-import {Modal} from '@/components/modal';
-import {Table} from '@/components/table';
-import {initSatellite} from '@junobuild/core-peer';
-import {useEffect} from 'react';
+import { Auth } from '@/components/auth'
+import { Background } from '@/components/background'
+import Footer from '@/components/landing/footer'
+import { Modal } from '@/components/modal'
+import { Table } from '@/components/table'
+import { initSatellite } from '@junobuild/core-peer'
+import { useEffect } from 'react'
 
 export default function Home() {
-  useEffect(() => {
-    (async () =>
-      await initSatellite({
-        workers: {
-          auth: true
-        }
-      }))();
-  }, []);
+    useEffect(() => {
+        ;(async () =>
+            await initSatellite({
+                workers: {
+                    auth: true,
+                },
+            }))()
+    }, [])
 
-  return (
-    <>
-      <div className="relative isolate min-h-[100dvh]">
-        <main className="mx-auto max-w-screen-2xl py-16 px-8 md:px-24 tall:min-h-[calc(100dvh-128px)]">
-          {/* <h1 className="dark:text-white text-5xl md:text-6xl font-bold tracking-tight md:pt-24">
+    return (
+        <>
+            <div className='relative isolate min-h-[100dvh]'>
+                <main className='tall:min-h-[calc(100dvh-128px)] mx-auto max-w-screen-2xl px-8 py-16 md:px-24'>
+                    {/* <h1 className="dark:text-white text-5xl md:text-6xl font-bold tracking-tight md:pt-24">
             Example App
           </h1>
           <p className="dark:text-white py-4 md:max-w-lg">
@@ -37,17 +37,17 @@ export default function Home() {
             , showcasing a practical application of these technologies.
           </p> */}
 
-          <Auth>
-            <Table />
+                    <Auth>
+                        <Table />
 
-            <Modal />
-          </Auth>
-        </main>
+                        <Modal />
+                    </Auth>
+                </main>
 
-        {/* <Footer /> */}
+                {/* <Footer /> */}
 
-        {/* <Background /> */}
-      </div>
-    </>
-  );
+                {/* <Background /> */}
+            </div>
+        </>
+    )
 }
