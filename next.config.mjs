943176efddab437beb4120db704fd0b6659a0233
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-10-20 03:33:43 +0200

    fix: disable juno for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
-import { withJuno } from '@junobuild/nextjs-plugin'
+// import { withJuno } from '@junobuild/nextjs-plugin'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { unoptimized: true },
-    output: 'export',
+    // images: { unoptimized: true },
+    // output: 'export',
     reactStrictMode: true,
 }
 
-export default withJuno({ nextConfig, juno: { container: true } })
+// export default withJuno({ nextConfig, juno: { container: true } })
+
+export default nextConfig
