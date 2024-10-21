8c6e01455ee6c5283751ce9c2a8fd31a770cc214
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-10-21 04:46:36 +0200

    fix: only show footer in home page

--- src/app/@footer/default.tsx
+++ src/app/@footer/default.tsx
@@ -1,3 +1,12 @@
-export default function DefaultFooter() {
+'use client'
+
+import Footer from '@/features/landing/components/footer'
+import { usePathname } from 'next/navigation'
+
+export default function HomeFooter() {
+    const pathname = usePathname()
+    if (pathname === '/') {
+        return <Footer />
+    }
     return null
 }
--- src/app/@footer/page.tsx
+++
@@ -1,5 +0,0 @@
-import Footer from '@/features/landing/components/footer'
-
-export default function HomeFooter() {
-    return <Footer />
-}
